5ab12ad795dbd81fd0961a63d1dd7d017e5fb786
Henrik Lindgren <lindgrendude@gmail.com>
2023-05-25 17:32:42 +0000

    bot code refactored

--- bot.py
+++ bot.py
@@ -1,159 +1,131 @@
-import json
-import discord
-from discord.ext import commands
-import aiohttp
+import os
+import re
 import base64
 import io
+import discord
+from discord.ext import commands
 import openai
-from dotenv import load_dotenv
-import os
+import aiohttp
 import argparse
-import re
+from dotenv import load_dotenv
 
-# Load environment variables from the .env file
+# Load environment variables
 load_dotenv()
 
 API_URL = os.getenv("API_URL")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+# Check if environment variables are set
+if not API_URL or not OPENAI_API_KEY or not BOT_TOKEN:
+    raise EnvironmentError('Environment variables not set correctly. Please check your .env file.')
+
+# Set OpenAI API key
 openai.api_key = OPENAI_API_KEY
 
-
-# Create a new bot instance
+# Create bot instance
 intents = discord.Intents.default()
-#intents.messages = True
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 
-def parse_command(commands):    
+def parse_command(commands):
+    """Parse the command string into separate arguments"""
     parser = argparse.ArgumentParser()
 
     # Define arguments
     parser.add_argument('--prompt', type=str, required=True)
-    parser.add_argument('--batch_size', type=int, required=False, default = 1)
-    parser.add_argument('--cfg_scale', type=int, required=False, default = 7)
-    parser.add_argument('--seed', type=int, required=False, default = -1)
-    parser.add_argument('--steps', type=int, required=False, default = 50)
-    
-    # Split the string at spaces, excluding spaces within quotes
-    split_list = re.findall(r'"[^"]+"|\S+', commands)
+    parser.add_argument('--batch_size', type=int, required=False, default=1)
+    parser.add_argument('--cfg_scale', type=int, required=False, default=7)
+    parser.add_argument('--seed', type=int, required=False, default=-1)
+    parser.add_argument('--steps', type=int, required=False, default=50)
 
-    # Remove the quotes from the split values
+    # Split string and remove quotes
+    split_list = re.findall(r'"[^"]+"|\S+', commands)
     split_list = [item.strip('"') for item in split_list]
     args = parser.parse_args(split_list)
 
     return args.__dict__
 
 
+async def send_images(images, ctx):
+    """Convert base64 images to files and send them"""
+    files = []
+    for i, image in enumerate(images):
+        try:
+            image_bytes = base64.b64decode(image)
+            image_file = io.BytesIO(image_bytes)
+            files.append(discord.File(image_file, f'image{i}.png'))
+        except Exception as e:
+            await ctx.reply(f'Error sending image: {e}')
+            return
+
+    await ctx.reply(files=files)
+
+
 @bot.event
 async def on_ready():
+    """On bot ready event handler"""
     print(f'Bot has connected to Discord!')
     for guild in bot.guilds:
         print(f'Connected to guild: {guild.name} (id: {guild.id})')
 
 
-# Define the 'hello' command
 @bot.command(name='hello')
 async def hello(ctx):
-    if ctx.message.author.id == int(os.getenv("NIBBE")): # nibbe
-        await ctx.reply('wassup nigga')
-    elif ctx.message.author.id == int(os.getenv("JEPPE")): # jeppe
-        await ctx.reply('sup')
-    elif ctx.author.id == int(os.getenv("HENKE")): # henke
-        await ctx.reply('whats crackin')
-    elif ctx.author.id == int(os.getenv("BULE")): # ludwig
-        await ctx.reply('hard stuck gold rofl lmao')
-    else:
-        await ctx.reply('yo yo yo yoy yo')
+    """Hello command handler"""
+    user_id = ctx.message.author.id
+    user_responses = {
+        os.getenv("NIBBE"): 'wassup nigga',
+        os.getenv("JEPPE"): 'sup',
+        os.getenv("HENKE"): 'whats crackin',
+        os.getenv("BULE"): 'hard stuck gold rofl lmao'
+    }
+    response = user_responses.get(user_id, 'yo yo yo yoy yo')
+    await ctx.reply(response)
 
-# Command: !diffusion
+
 @bot.command()
 async def diffusion(ctx, *, args):
-    # This function will be called when '!diffusion' command is used in any channel the bot has access to
-
+    """Diffusion command handler"""
     try:
         payload = parse_command(args)
-        success = True
-    except SystemExit:
-        await ctx.reply(f'Error: Incorrect command usage.\n!diffusion --prompt "kexchoklad" --steps 10 --batch_size 1 --cfg_scale 7')
-        success = False
-    except Exception as e:
-        await ctx.reply(f'Error: {e}')
-        success = False
-
-    if success:
         await ctx.message.add_reaction('\U0001F440')
         async with aiohttp.ClientSession() as session:
             async with session.post(f'{API_URL}/sdapi/v1/txt2img', json=payload) as response:
-                # Add reaction to acknowledge the message
                 r = await response.json()
-
-                # Check if the request was successful
                 if response.status == 200:
-                    # If successful, send the response data back to the channel
-                    # Extract data from response
-                    images = r.get('images', [])
-                    parameters = r.get('parameters', {})
-                    info = r.get('info', '')
-                    files = []
-                    # Send images to the channel
-                    for i, image in enumerate(images):
-                        try:
-                            # Try to decode the base64 string and create an image file
-                            image_bytes = base64.b64decode(image)
-                            image_file = io.BytesIO(image_bytes)
-                            files.append(discord.File(image_file, f'image{i}.png'))
-                        except Exception as e:
-                            # If there's an error, send a message
-                            await ctx.reply(f'Error sending image: {e}')
-                            return
-
-                    # Send all images in one message
-                    await ctx.reply(files=files)
-                    # Send parameters to the channel
-                    #await ctx.send(f'Parameters: {parameters}')
-                    # Send info to the channel
-                    #await ctx.send(f'Info: {info}')
-
+                    await send_images(r.get('images', []), ctx)
                 else:
-                    # If not successful, send an error message
                     await ctx.reply(f'Error: {response.status}')
+    except SystemExit:
+        await ctx.reply(f'Error: Incorrect command usage.\n!diffusion --prompt "kexchoklad" --steps 10 --batch_size 1 --cfg_scale 7')
+    except Exception as e:
+        await ctx.reply(f'Error: {e}')
 
 
-# Command: !chat
 @bot.command()
 async def chat(ctx, *args):
-    # This function will be called when '!chat' command is used in any channel the bot has access to
+    """Chat command handler"""
     arg = ' '.join(args)
-    # Define the payload
-    MODEL = "gpt-3.5-turbo"
+    model = "gpt-3.5-turbo"
     try:
-        # Send the API request
-        
         response = openai.ChatCompletion.create(
-            model = MODEL,
-            messages = [
-                {"role": "system","content":"You are a helpful assistant."},
-                {"role": "user","content":arg}
+            model=model,
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": arg}
             ]
         )
 
-        # Extract the response
         chat_response = response['choices'][0]['message']['content']
-        # Send the chat response to the channel
-        max_message_length = 2000  # Maximum length of a single message
+        max_message_length = 2000
         while len(chat_response) > max_message_length:
             await ctx.reply(chat_response[:max_message_length])
             chat_response = chat_response[max_message_length:]
         await ctx.reply(chat_response)
-
-
     except openai.error.InvalidRequestError as e:
-        # If there's an error, send a message
         await ctx.reply(f'Error: {str(e)}')
-
 
 
 # Run the bot
