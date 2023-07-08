1e0c994e215aff16e8a2b1dc787b65ccc44fd288
Henrik Lindgren <lindgrendude@gmail.com>
2023-07-08 20:33:08 +0000

    bot works

--- README.md
+++ README.md
@@ -6,4 +6,9 @@ docker build -t my-python-app .
 
 docker run -d --name my-running-app my-python-app
 
+docker compose --profile download up --build
+
 docker compose --profile auto up --build -d
+
+
+docker system prune -a
--- bot.py
+++ bot.py
@@ -9,6 +9,9 @@ import aiohttp
 import argparse
 from dotenv import load_dotenv
 import asyncio
+import time
+import requests
+import json
 
 # Load environment variables
 load_dotenv()
@@ -41,12 +44,17 @@ def parse_command(commands):
     parser.add_argument('--batch_size', type=int, required=False, default=1)
     parser.add_argument('--cfg_scale', type=int, required=False, default=7)
     parser.add_argument('--seed', type=int, required=False, default=-1)
-    parser.add_argument('--steps', type=int, required=False, default=50)
+    parser.add_argument('--steps', type=int, required=False, default=25)
 
     # Split string and remove quotes
     split_list = re.findall(r'"[^"]+"|\S+', commands)
     split_list = [item.strip('"') for item in split_list]
     args = parser.parse_args(split_list)
+
+    # Apply constraints
+    args.steps = max(1, min(args.steps, 50))
+    args.cfg_scale = max(1, min(args.cfg_scale, 30))
+    args.batch_size = max(1, min(args.batch_size, 10))
 
     return args.__dict__
 
@@ -64,6 +72,22 @@ async def send_images(images, ctx):
             return
 
     await ctx.reply(files=files)
+
+
+@bot.command()
+async def info(ctx):
+    """Info command handler"""
+    await ctx.reply(
+        f'The `!diffusion` command is used to generate images based on the provided prompt.\n'
+        f'Usage: `!diffusion --prompt "your prompt here" [--steps num_steps] [--batch_size num] [--cfg_scale scale]`\n'
+        f'Arguments:\n'
+        f'`--prompt`: (required) The text prompt based on which the image is to be generated.\n'
+        f'`--steps`: (optional) Number of steps to be used for the diffusion process. Default is 25. Must be between 1 and 50 (inclusive).\n'
+        f'`--batch_size`: (optional) Size of the batch for the diffusion process. Default is 1. Must be between 1 and 10 (inclusive).\n'
+        f'`--cfg_scale`: (optional) Scale configuration for the diffusion process. Default is 7. Must be between 1 and 30 (inclusive).\n'
+        f'\nExample: `!diffusion --prompt "sunset over the mountains" --steps 25 --batch_size 2 --cfg_scale 7`\n'
+        f'This command will generate an image based on the prompt "sunset over the mountains" using 25 steps, a batch size of 2, and a scale configuration of 7.\n'
+    )
 
 
 @bot.event
@@ -111,12 +135,21 @@ async def hello(ctx):
 @bot.command()
 async def diffusion(ctx, *, args):
     """Diffusion command handler"""
-    await ctx.message.add_reaction('\U0001F440')
+
+    start_time = time.time() # start a timer
+
+    await ctx.message.add_reaction('\U0001F440') # side eye emoji
+    if semaphore.locked():
+        await ctx.message.add_reaction('\u23F2')  # Clock emoji
     await semaphore.acquire()
+    await ctx.message.remove_reaction('\u23F2', bot.user)
+
     fetch_task = bot.loop.create_task(fetch_progress(ctx,ctx.message))
+
     try:
         print(f'Starting diffusion request from {ctx.message.author} with args: {args}')
         payload = parse_command(args)
+        prompt = payload['prompt']
 
         # Start the progress fetching coroutine
         async with aiohttp.ClientSession() as session:
@@ -130,6 +163,29 @@ async def diffusion(ctx, *, args):
         await ctx.reply(f'Error: Incorrect command usage.\n!diffusion --prompt "kexchoklad" --steps 10 --batch_size 1 --cfg_scale 7')
     except Exception as e:
         await ctx.reply(f'Error: {e}')
+    
+    while True:
+        found_me = 0
+        for reaction in ctx.message.reactions:
+            if reaction.me:
+                await reaction.remove(bot.user)
+                found_me = 1
+                break
+        if found_me:
+            continue
+        else:
+            break
+    # Remove all reactions
+    #await ctx.message.clear_reactions()
+    #await ctx.message.reactions[0].remove(bot.user)
+    await ctx.message.add_reaction('\u2705')  # Checkmark emoji
+
+    # Calculate the duration
+    duration = time.time() - start_time
+    
+    # Reply with the duration
+    #await ctx.reply(f"It took me {duration:.2f} seconds to create an image based on '{prompt}'.")
+
     fetch_task.cancel()  # Cancel the fetch_progress coroutine
     semaphore.release()
 
@@ -156,6 +212,16 @@ async def chat(ctx, *args):
         await ctx.reply(chat_response)
     except openai.error.InvalidRequestError as e:
         await ctx.reply(f'Error: {str(e)}')
+
+
+
+@bot.command(name='meme')
+async def meme(ctx):
+    response = requests.get('https://meme-api.com/gimme')
+    json_data = json.loads(response.text)
+    memes = json_data['url']
+
+    await ctx.send(memes)
 
 
 # Run the bot
